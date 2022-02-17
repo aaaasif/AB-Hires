9e1ad5b3ff5e1ca2d28b0b4b74cd9110acd97ca7
aaaasif <alexasif4gmail.com>
2022-02-17 15:00:52 +0600

    footer updated

---
+++ src/Components/LatestJob/LatestJob.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './LatestJob.css'
+
+const LatestJob = () => {
+    return (
+        <div>
+            <h1 className='jobHeader'>Latest Job</h1>
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LatestJob;
